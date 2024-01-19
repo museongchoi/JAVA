b24d34f0b428165d6cca064bbe02e82e4107b223
museongchoi <97529960+museongchoi@users.noreply.github.com>
2024-01-19 17:07:05 +0900

    programmers build intellij

--- Programmers/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.programmers {
-    requires javafx.controls;
-    requires javafx.fxml;
-            
-                            
-    opens com.example.programmers to javafx.fxml;
-    exports com.example.programmers;
-}
