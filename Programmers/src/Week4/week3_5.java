4b810e8d9fa065f481e8586b4ddf38f12217c49d
museongchoi <97529960+museongchoi@users.noreply.github.com>
2024-01-19 17:27:45 +0900

    1

---
+++ Programmers/src/Week4/week3_5.java
@@ -0,0 +1,14 @@
+package Week4;
+
+class Solution {
+    public int solution(int a, int b) {
+        int answer = 0;
+
+        String ab = Integer.toString(a) + Integer.toString(b);
+        int ba1 = 2 * a * b;
+
+        answer += Math.max(Integer.parseInt(ab), ba1);
+
+        return answer;
+    }
+}
